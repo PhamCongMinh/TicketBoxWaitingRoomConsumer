6ba97f123ec793873faabee7646744c5be23fa43
PhamCongMinh <minhctthvn@gmail.com>
2024-12-03 11:43:22 +0700

    fix: add logic send message create and delete ticket type to kafka

--- src/main/java/org/ticketbox/service/ticket_type/TicketTypeService.java
+++ src/main/java/org/ticketbox/service/ticket_type/TicketTypeService.java
@@ -1,28 +1,35 @@
 package org.ticketbox.service.ticket_type;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.ticketbox.database.model.Event;
 import org.ticketbox.database.model.TicketType;
-import org.ticketbox.database.repository.EventRepository;
 import org.ticketbox.database.repository.TicketTypeRepository;
-import org.ticketbox.shared.constant.ErrorCodeConstant;
-import org.ticketbox.shared.exception.custom.BadRequestException;
+import org.ticketbox.service.kafka.KafkaProducerService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class TicketTypeService {
     @Autowired
     private TicketTypeRepository ticketTypeRepository;
+    private final KafkaProducerService kafkaProducerService;
+    private static final Logger log = LoggerFactory.getLogger(TicketTypeService.class.getSimpleName());
 
     public List<TicketType> getTicketTypesByEvent(Integer eventId) {
         return ticketTypeRepository.findByEventId(eventId);
     }
 
     public TicketType createTicketType(TicketType ticketType) {
-        return ticketTypeRepository.save(ticketType);
+        TicketType newTicketType = ticketTypeRepository.save(ticketType);
+        String topic = "create_ticket_type";
+        String testMessage = "test";
+        kafkaProducerService.sendMessage(topic, testMessage);
+        log.info("Send message create Ticket Type to topic: " + topic);
+        return newTicketType;
     }
 
 //    public Event editTicketType(Integer id, Event event) {
@@ -35,5 +42,9 @@ public class TicketTypeService {
 
     public void deleteTicketTypeById(Integer id) {
         ticketTypeRepository.deleteById(id);
+        String topic = "delete_ticket_type";
+        String testMessage = "test";
+        kafkaProducerService.sendMessage(topic, testMessage);
+        log.info("Send message delete Ticket Type to topic: " + topic);
     }
 }
